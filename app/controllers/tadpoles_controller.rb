08341745ea03a07c8d4d9cc928a3443ffd2562c7
Laura Felce <crepu83@yahoo.it>
2019-07-02 15:13:09 +0000

    Automatically backed up by Learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -2,6 +2,7 @@ class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphose]
 
   def metamorphose
+    @tadpole = Tadpole.find(set_tadpole)
   end  
 
   def index
